131251d999fe3eda0e300b5c95cd6f23a234e65d
Kristofer Joseph <kristofer@kristoferjoseph.com>
2010-06-09 08:28:53 -0700

    Stubbed out template, need tests

--- lib/generators/command_generator.rb
+++ lib/generators/command_generator.rb
@@ -3,14 +3,51 @@ module Sprout
   class CommandGenerator < Generator::Base
     
     ##
-    # This is how you add a parameter to your generator
-    # add_param :fwee, String, { :default => "fwee" }
+    # The path where source files should be created.
+    add_param :src, String, { :default => 'src' }
 
     def manifest
       directory input.snake_case do
-        template input.camel_case
+        template "#{input.camel_case}.as", 'Command.as'
       end
       
+    end
+    
+    def class_directory
+      parts = input_in_parts
+      if parts.size > 1
+        parts.pop
+        return File.join src, *parts
+      end
+      return src
+    end
+
+    def package_name
+      parts = input_in_parts
+      if parts.size > 1
+        parts.pop
+        return "#{parts.join('.')} "
+      end
+      return ""
+    end
+
+    def class_name
+      parts = input_in_parts
+      parts.pop.camel_case
+    end
+
+    def input_in_parts
+      provided_input = input
+      if provided_input.include?('/')
+        provided_input.gsub! /^#{src}\//, ''
+        provided_input = provided_input.split('/').join('.')
+      end
+
+      provided_input.gsub!(/\.as$/, '')
+      provided_input.gsub!(/\.mxml$/, '')
+      provided_input.gsub!(/\.xml$/, '')
+
+      provided_input.split('.')
     end
 
   end
--- lib/generators/templates/Command.as
+++ lib/generators/templates/Command.as
@@ -1,1 +1,15 @@
-This is your generator template
+package <%= package_name %>
+{
+	import org.robotlegs.mvcs.Command;
+	
+	public class <%= class_name %> extends Command
+	{
+		/**
+		* @inheritDoc
+		*/
+		override public function execute():void
+		{
+			
+		}
+	}
+}
