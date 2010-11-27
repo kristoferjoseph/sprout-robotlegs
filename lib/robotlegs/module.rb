f6426055bfe0b6573ccf20f291be7e5b839ace75
Kristofer Joseph <kristofer@kristoferjoseph.com>
2010-11-27 13:58:59 -0800

    --amend

---
+++ lib/robotlegs/module.rb
@@ -0,0 +1,9 @@
+module Robotlegs  
+  NAME    = 'robotlegs'
+  ZIP_VERSION = '1.4.0'
+  ZIP_MD5 = '2c0962ea2f5e9665d1ac8951c7448b95'
+  # Version applies directly to Robotlegs official version. To release patches to 
+  # the sprout or gem, increment the micro value, or the 4th number in the version sequence.
+  version_file = File.join(File.dirname(__FILE__), '..', '..', 'VERSION')
+  VERSION = File.read(version_file).strip 
+end
